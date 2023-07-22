112d42ce7e892097ece4a2e7eb48a2318919b639
ojmedrano <osmeg92@hotmail.com>
2023-07-22 11:21:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberText = document.getElementById("memberText");
+    var skillsText = document.getElementById("skillsText");
+    var projectsText = document.getElementById("projectsText");
+    var contactText = document.getElementById("contactText");
+    var aboutText = document.getElementById("aboutText");
+    member.style.color = "#fff";
+    skills.style.color = "#000";
+    projects.style.color = "#000";
+    contact.style.color = "#000";
+    about.style.color = "#000";
+    memberText.style.display = "block";
+    skillsText.style.display = "none";
+    projectsText.style.display = "none";
+    contactText.style.display = "none";
+    aboutText.style.display = "none";
+}
